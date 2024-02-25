7723abcac3e95d3766c3b3a653289c9b1f78f79a
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-02-26 01:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const makeChange = (c) => {
-  // your name here
+	// your name here
+	const quarters = Math.floor(c / 25);
+	c %= 25;
+	const dimes = Math.floor(c / 10);
+	c %= 10;
+	const nickels = Math.floor(c / 5);
+	c %= 5;
+	const pennies = c;
+	return { q: quarters, d: dimes, n: nickels, p: pennies };
 };
-
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
